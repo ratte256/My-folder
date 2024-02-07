10d930e0b65454e042369416200ea59f013e133a
ratte256 <156171496+ratte256@users.noreply.github.com>
2024-02-08 08:51:59 +0900

    update "souse.c" files.

--- c language/File-related-prototyping/souse.c
+++ c language/File-related-prototyping/souse.c
@@ -21,3 +21,10 @@ printf("Do you want to read the file[0]? \n Do you want to write [1]? \n Are you
 
 return 0;
 }
+
+
+/*関係ない話だけど、bing検索で出てくる”https://www.bing.com/search?q=%E7%BF%BB%E8%A8%B3&aqs=edge.0.69i59j69i65j69i61l2.5852j0j1&pglt=43&FORM=ANCMS9&PC=U531”で
+英語の”HeHeHe  birthed day is Tenmon 3 years may 12th.”
+を日本語に翻訳すると
+”へへへぇぇ��”になるバグがあってんだけど
+原因はたぶん”HeHeHe”だよね。*/
