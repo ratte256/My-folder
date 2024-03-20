0b1a32ce45651f9d5e318b9544774d71fac69a1d
ratte256 <156171496+ratte256@users.noreply.github.com>
2024-03-20 19:44:37 +0900

    Update minb.h
    minb is not best.

--- c language/Original c sourse hedders/minb.h
+++ c language/Original c sourse hedders/minb.h
@@ -1,8 +1,8 @@
 #ifndef MINB_H
 #define MINB_H
 
-int RNG(int RNG_index)
+int RNG(int RNG_index, int Bit)
 {
-    RNG_index = RNG_index * 5 + 1 % 4096;
+    RNG_index = RNG_index * 5 + 1 % Bit;
     return 0;
 }
